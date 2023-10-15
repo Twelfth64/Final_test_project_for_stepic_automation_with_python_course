57065ef56b5b042bd5ba8ac61d440fd9e03700a6
Twelfth641 <twelfth641@gmail.com>
2023-10-15 18:21:56 +0300

    Add test_guest_should_see_login_link test.Add assert method for noSuchElement exception

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as Chrome_options
+from selenium.webdriver.firefox.options import Options as Firefox_options
 
 
 def pytest_addoption(parser):
@@ -18,12 +18,12 @@ def browser(request):
     user_language = request.config.getoption("language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        options = Options()
+        options = Chrome_options()
         options.add_argument("lang="+user_language)
-        browser = webdriver.Chrome(options=options, executable_path="C:\chromedriver")
+        browser = webdriver.Chrome(options=options, executable_path="\chromedriver")
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        options = Options()
+        options = Firefox_options()
         options.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(options=options)
     else:
