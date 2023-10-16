7c14736f95ea7b024abdde233dc27cadc577a651
Twelfth641 <twelfth641@gmail.com>
2023-10-17 01:25:49 +0300

    Add locators for ProductPage. Added few methods for product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_BUTTON = (By.CSS_SELECTOR, "[name='login_submit']")
     REGISTRATION_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
+
+class ProductPageLocators():
+        ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '''[class*='btn-add-to-basket"]''')
+    TITLE_OF_PRODUCT = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    TITLE_OF_ADDED_TO_CART_PRODUCT = (By.CSS_SELECTOR, ".alertinner strong")
---
+++ test_product_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from .pages.product_page import ProductPage
+import time
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
+    page.solve_quiz_and_get_code()
+    time.sleep(1000)
